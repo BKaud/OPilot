dad858eb98ba66c10359cef3adecd62a659017d8
Braden Kauderer <bkauderer26@gmail.com>
2025-10-07 23:07:59 -0400

    First changes in a while

--- OperatorRotationApi/obj/Debug/net8.0/OperatorRotationApi.AssemblyInfo.cs
+++ OperatorRotationApi/obj/Debug/net8.0/OperatorRotationApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorRotationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d2cb1affce81ff24673c0ecd7b15dfac551457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f960b52b809fa97a7f454b13942d64e55395743")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorRotationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorRotationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
